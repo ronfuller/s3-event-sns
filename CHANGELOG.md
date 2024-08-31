f56cf5767b31d562fd8338f7176c95ad278ff49e
Ron Fuller <ronfuller.330@gmail.com>
2024-08-30 20:27:28 -0700

    initial commit

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 # Changelog
 
-All notable changes to `:package_name` will be documented in this file.
+All notable changes to `s3-event-sns` will be documented in this file.
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace Psi\S3EventSns\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
